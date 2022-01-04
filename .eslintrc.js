cd0a771f9fbb1d2192ae1eaedc367c1b57e798cb
CJ <cj@bluebird.dev>
2022-01-04 09:59:23 +0400

    feature: add tailwind

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
   extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        singleQuote: true,
+        printWidth: 120,
+        tabWidth: 2,
+      },
+    ],
+  },
 };
